9b577bd772985703c5b8352fd6d0762966710215
Milan Orlović <milanorlovic1982@gmail.com>
2023-07-12 14:01:04 +0200

    Published at

--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -1,7 +1,42 @@
 require "test_helper"
 
 class BlogPostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'draft? returns true for draft posts ' do
+  #binding.irb debuging 
+  assert blog_posts(:draft).draft?
+  end
+
+  test 'draft? returns false for published posts ' do
+  refute blog_posts(:published).draft?
+  end
+
+
+  test 'draft? returns false for scheduled posts ' do 
+    refute BlogPost.new(published_at: 1.year.from_now ).draft?
+    end
+
+    test 'published? return true for published blog posts' do
+    assert BlogPost.new(published_at: 1.day.ago).published? 
+    end
+    test 'published? return false for draft blog posts' do
+      refute BlogPost.new(published_at: nil).published? 
+      
+    end
+    test 'published? return false for scheduled blog posts' do
+      refute  BlogPost.new(published_at: 1.day.from_now ).published? 
+      
+    end
+    test 'scheduled? return true for scheduled blog posts' do
+      assert BlogPost.new(published_at: 1.day.from_now).scheduled? 
+      end
+      test 'scheduled? return false for draft blog posts' do
+        refute BlogPost.new(published_at: nil).scheduled? 
+        
+      end
+      test 'scheduled? return false for published blog posts' do
+        refute  BlogPost.new(published_at: 1.day.ago ).scheduled? 
+        
+      end
+
 end
